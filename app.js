12ceac5956f21cb80b3f89d574225cff6b4c1713
Eshanaaz <eshanaz76@gmail.com>
2021-11-09 03:06:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,492 @@
+
+                                            ///////////////ARRAYS/////////////////////
+
+
+// ///////QUESTION  1/////////
+
+
+// var stdArr1=[];
+// stdArr1=[]
+// document.write(stdArr1)
+
+
+
+// document.write("<br>")
+
+
+// // ///////QUESTION  2/////////
+
+
+// var stdArr2=[];
+// stdArr2=[]
+// document.write(stdArr2)
+// document.write("<br>")
+
+
+// // ///////QUESTION  3/////////
+
+
+
+// var forStr=[];
+// forStr=["ESHA NAZ"]
+// document.write(forStr)
+// document.write("<br>")
+
+
+
+// // ///////QUESTION  4/////////
+
+
+
+// // var forNum=[];
+// // forNum=[30, 40, 50, 90]
+// // document.write(forNum [2])
+// // document.write("<br>")
+
+
+// // ///////QUESTION  5/////////
+
+
+
+// // var booL=[];
+// // booL=[true]
+// // document.write(booL)
+// // document.write("<br>")
+
+// // ///////QUESTION  6/////////
+
+
+// // var mixedArr = ["ESHA NAZ" , 20 , true ]
+
+// // document.write(mixedArr[0])
+
+// // document.write("<br>")
+
+
+
+// // ///////QUESTION  7/////////
+// document.write("QUALIIFICATONS; <br>")
+// document.write("<br>")
+// document.write("<br>")
+
+
+// var quaLif=["1)SSC<br> 2)HSC<br>  3)BCS<br>  4)BS<br>  5)BCOM<br> 6)MS<br> 7)M. Phil.<br> 8)PhD<br>"]
+// document.write(quaLif)
+// document.write("<br>")
+
+
+
+// // ///////QUESTION  8/////////
+
+// var stdName=["Hamza", "Esha", "Tooba"]
+// var scOre=[330, 490, 220]
+// var per=[330/500*100, 490/500*100, 220/500*100]
+
+// document.write("Score of"+ " "+ stdName[0]+" "+ "is"+ " "+ scOre[0] +"."+"Percentage"+ " "+ per[0]+ "%" )
+// document.write( " <br> Score of"+ " "+ stdName[1]+" "+ "is"+" "+ scOre[1] +"."+"Percentage"+ " "+ per[1] + "%" )
+// document.write("<br> Score of"+ " "+ stdName[2]+" "+ "is"+" "+ scOre[2] +"."+"Percentage"+ " "+ per[2]+ "%" )
+// document.write("<br>")
+
+// document.write("<br>")
+// document.write("<br>")
+
+
+// // ///////QUESTION  9/////////
+
+// var colourAvai=["<b> AVAILABLE COLOURS: </b>" + " "+ "Black", "Red","Pink","White","Purple", "Orange"]
+// document.write(colourAvai)
+// document.write("<br>")
+
+
+
+// ////a/////
+
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"]
+
+// var colorAd=prompt("What colour u wanna add in the begining?")
+// colorName.unshift(colorAd)
+// document.write("<b>ADD COLOUR IN THE BEGINING</b>" + " "+ colorName)
+// document.write("<br>")
+
+
+// /////b//////
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"]
+ 
+// var colorAd=prompt(  "What colour u wanna add to the last?")
+// colorName.push(colorAd)
+// document.write("<b> ADD COLOUR IN THE BEGINING </b>" + " " +colorName)
+// document.write("<br>")
+
+
+// /////c//////
+
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"]
+ 
+// var colorAd=prompt(  "What TWO colourS u wanna add in the begining?")
+// colorName.unshift(colorAd)
+// document.write("<b>ADD TWO COLOUR IN THE BEGINING</b>" + " "+ colorName)
+// document.write("<br>")
+
+
+// /////d//////
+
+
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"]
+ 
+// colorName.shift([0])
+// document.write("<b>Deleting first colour</b>" + " "+ colorName)
+// document.write("<br>")
+
+////////e////////
+
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"]
+ 
+// colorName.pop([5])
+// document.write("<b>Deleting last colour</b>" + " "+ colorName)
+// document.write("<br>")
+
+// ////////f////////
+
+// var clrNme=prompt("What first colour u wanna add in the list" + " <br>")
+// var clrpos=+prompt("At where you wants to add a color" + " <br>")
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"];
+ 
+
+// colorName.splice(2,0, "Orange" ,"Yellow")
+
+// document.write(colorName)
+
+// document.write("<br>")
+
+
+
+///////g/////////
+
+// var colorName=["Black", "Red","Pink","White","Purple", "Orange"];
+ 
+
+// colorName.splice(2,1,)
+
+// document.write(colorName)
+
+// document.write("<br>")
+
+
+
+// document.write("<br>")
+
+// // ///////QUESTION  10/////////
+
+// var scrOfstd=[300, 500, 100,220,700]
+// document.write( "Scores of Students:" + " "  + scrOfstd + " <br>")
+// document.write ("Ordered Scores of students:" + scrOfstd[2]+ ","+ " ")
+// document.write (scrOfstd[3]+ ","+ " " )
+// document.write (scrOfstd[0]+ ","+ " ")
+// document.write (scrOfstd[1]+ ","+ " ")
+// document.write (scrOfstd[4]+ ","+ " ")
+
+// document.write("<br>")
+
+// // ///////QUESTION  11/////////
+
+// var citiesList=["Cities List:"+ " "+ "Karachi","Lahore","Islamabad","Queeta","Peshawar"]
+// document.write(citiesList);
+
+// document.write("<br> Slected cities:" + " "+  citiesList[2] + " ," + " ")
+// document.write( citiesList[4])
+
+// document.write("<br>")
+
+
+// // ///////QUESTION  12/////////
+
+
+// var arr = ["This "  ,  " is " ,    "my"  ,    " cat"];
+// document.write( "ARRAY:"+ " " + arr)
+// document.write("<br> STRING:"+ arr[0] )
+// document.write( arr[1] )
+// document.write( arr[2] )
+// document.write( arr[3] )
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+
+
+// // ///////QUESTION  13/////////
+
+// var fiFo=["Keyboard", " Mouse", "Printer", "Monitor"]
+// document.write("<h3>Devices:</h3>"+ fiFo + " <br>")
+// document.write("Out: <br>"+  fiFo[0] + "<br>")
+// document.write("Out: <br>"+  fiFo[1]+ "<br>")
+// document.write("Out: <br>"+  fiFo[2]+ "<br>")
+// document.write("Out: <br>"+  fiFo[3]+ "<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+
+// // ///////QUESTION  14/////////
+
+// var fiFo=["Keyboard", " Mouse", "Printer", "Monitor"]
+// document.write("<h3>Devices:</h3>"+ fiFo + " <br>")
+// document.write("Out: <br>"+  fiFo[3] + "<br>")
+// document.write("Out: <br>"+  fiFo[2]+ "<br>")
+// document.write("Out: <br>"+  fiFo[1]+ "<br>")
+// document.write("Out: <br>"+  fiFo[0]+ "<br>")
+// document.write("<br>")
+
+// // ///////QUESTION  15/////////
+
+// document.write("<h4>Dropdown</h4>")
+// var phNames=["Apple","Samsung", "Motorola", "Nokia", "Sony" +"&" +" Haier"]
+// document.write("<select><option>"+ phNames+"</select></option>")
+
+
+
+
+                                              ///////ARRAYS AND LOOPS////////////////////
+
+
+// // //////QUESTION 1//////////
+
+// var fruits=[]
+// fruits[0]="apple"
+// fruits[1]="apple"
+// fruits[2]="apple"
+// document.write(fruits)
+
+
+// //////QUESTION 2//////////
+
+
+// //////QUESTION 03//////////
+// // Write a program to print numeric counting from 1 to 10                       
+
+
+
+// for(var i = 1 ; i <= 10 ; i++ ){
+
+//     document.write( i + '<br>')
+
+
+// }
+
+
+
+// //////QUESTION 03//////////
+
+// //  Write a program to print multiplication table of any number using for loop. Table number & length should be 
+// // taken as an input from user.                 
+
+
+// var tableNumber=+prompt("Table number of your choice", "Table number")
+// var tableLenght=+prompt("Table lenght should be..", "Table lenght")
+
+
+// document.write("Table number:" + " " +tableNumber)
+// document.write(" <br> Table lenght:"  + " "+ tableLenght)
+// document.write("<br>")
+// document.write("<br>")
+// for(var i=1; i<= tableLenght; i++){
+//     document.write(tableNumber+ + " "+ "x"  + i+ " "+ "=" + " "+ tableNumber*i+"<br>")
+// } 
+
+
+
+// //////QUESTION 04//////////
+
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+                
+// for(var i = 0 ; i < fruits.length ; i++){
+//         document.write( fruits[i] + "<br>"); 
+// }
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+// document.write("Element at index 0 is apple");
+// document.write("<br> Element at index 1 is banana");
+// document.write("<br> Element at index 2 is mango");
+// document.write("<br> Element at index 3 is orange");
+// document.write("<br> Element at index 4 is strawberry");
+
+
+
+
+
+
+// //////QUESTION 05//////////
+
+// document.write("Counting")
+// document.write("<br>")
+// document.write("<br>")
+
+// for(var i = 1; i <=15 ; i = i + 1 ){
+
+//     document.write(i + " ")
+// }
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+// document.write("Reverse Counting")
+// document.write("<br>")
+// document.write("<br>")
+
+// for(var i = 10; i >=1 ; i = i-1 ){
+
+//     document.write(i+ " ," + " ")
+// }
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+
+// document.write("Even")
+// document.write("<br>")
+// document.write("<br>")
+
+// for(var i = 0; i <=20 ; i = i+2 ){
+
+//     document.write(i + " ,"+ " ")
+// }
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+// document.write("Odd")
+// document.write("<br>")
+// document.write("<br>")
+
+// for(var i = 1; i <=19 ; i = i+2 ){
+
+//     document.write(i + " ," + " ")
+// }
+// document.write("<br>")
+// document.write("<br>")
+
+
+// document.write("Series")
+// document.write("<br>")
+// document.write("<br>")
+
+// for(var i = 2; i <=18 ; i = i+2 ){
+
+//     document.write(i +"k"+ " ," + " ")
+// }
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+
+// //////QUESTION 06//////////
+
+
+// a
+
+// var num1 =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+// for( var i = 1; i <= num1.length; i++
+
+// ){
+//     document.write(num1)
+//     break;
+// }
+
+//b
+
+
+// var num1 =[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+// for( var i = 10; i <= num1.length; i--
+
+// ){
+//     document.write(num1)
+//     break;
+// }
+
+// //////QUESTION 07//////////
+
+
+// var items = ["cake" , "apple pie", "cookie", "chips", "patties"]
+
+// var userInput = prompt("Welcome to Boss Bakery..What do u wanna order Sir/Ma'am ?")
+
+// var matchFound = "no"
+// for(var i = 0 ; i < items.length ;i++){
+
+//         if(userInput === items[i] ){
+//             document.write(items [i] + " " + "is available in our bakery at index number " + [i] )
+//                 matchFound = "yes"
+//         } 
+// }
+
+// if(matchFound === "no"){
+//     document.write("We are sorry." + items [i]+" "+  "is not available in our bakery")
+// }
+
+// //////QUESTION 08//////////
+
+
+
+// var Arr=[24, 53, 78, 91, 12]
+// document.write("Array items:" + Arr)
+// document.write(" <br> The largest number is 91")
+
+
+// //////QUESTION 09//////////
+
+
+// var Arr=[24, 53, 78, 91, 12]
+// document.write("Array items:" + Arr)
+// document.write(" <br> The smallest number is 12")
+
+
+
+// //////QUESTION 10//////////
+
+
+
+// for ( var i=5; i<=100; i=i*5 ){
+//     document.write(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
